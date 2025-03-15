abce6daf486dbe2a2b674faa521ab78bb24c470b
TheJegede <112409241+TheJegede@users.noreply.github.com>
2025-03-15 15:27:51 -0400

    Update feedback.js

--- js/feedback.js
+++ js/feedback.js
@@ -25,7 +25,9 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 console.log(authToken)
             // Define API Gateway URL (Replace with your actual API endpoint)
-            const apiUrl = "https://an918u267j.execute-api.us-east-1.amazonaws.com/dev";
+            // const apiUrl = "https://an918u267j.execute-api.us-east-1.amazonaws.com/dev";
+            const apiUrl = url: _config.api.invokeUrl + '/StudentFeedback'
+console.log(apiUrl)
             // Prepare request payload
             const requestBody = {
                 enumber: enumber,
