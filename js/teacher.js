7d721be72d04e190b2a7b001fe53fa5280694032
DevMaxwell <anoliefomaxwell2@gmail.com>
2025-04-07 13:52:23 -0700

    fix - course response not populating on client

--- js/teacher.js
+++ js/teacher.js
@@ -1,5 +1,4 @@
 window.onload = async function () {
-  // Get teacher's email from localStorage
   var email = localStorage.getItem("teacherEmail");
 
   if (email) {
@@ -10,22 +9,19 @@ window.onload = async function () {
     window.location.href = "/index.html";
   }
 
-  // function to fetch courses
   async function fetchCoursesForEmail(email) {
     const apiUrl = `https://415ey95x52.execute-api.us-east-1.amazonaws.com/prod/courses/${email}`;
 
     try {
-      // Get Cognito authentication token
       const authToken = await WildRydes.authToken;
       if (!authToken) {
         alert("Authentication failed. Please log in again.");
         window.location.href = "/index.html";
         return;
       }
+
       console.log("Authentication Token:", authToken);
       console.log("API URL:", apiUrl);
-
-      // Send request to API Gateway
       const response = await fetch(apiUrl, {
         method: "GET",
         headers: {
@@ -34,21 +30,22 @@ window.onload = async function () {
         },
       });
 
+      const responseData = await response.json();
+
       if (!response.ok) {
-        console.log("error response " + (await response.json()));
+        console.error("Error response:", responseData);
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const responseData = await response.json();
-
       console.log("Courses fetched:", responseData);
 
-      // Get the course list element
+      const courses = JSON.parse(responseData.body);
+      console.log("Parsed course array:", courses);
+
       const courseList = document.getElementById("courseList");
       courseList.innerHTML = "";
 
-      // Loop through the courses and append them to the list
-      responseData.forEach((course) => {
+      courses.forEach((course) => {
         const li = document.createElement("li");
         li.className = "course-item";
 
